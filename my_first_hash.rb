abce7bf172b162ebe67d4ba01dadff50bd0c6e80
Derek phung <dragevards@gmail.com>
2020-03-31 20:43:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,11 @@
 def my_hash
   your_hash = {"name" => "derek"}
-  
+
 end
 
 
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
 end
 
 def retrieval
